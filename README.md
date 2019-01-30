77a9ada80d3e68d4bff9588d09129ebdcf1686ef
cavid24 <Bitbucket9099xy>
2019-01-30 17:51:26 +0400

    Updating readme

--- README.md
+++ README.md
@@ -10,3 +10,6 @@
 2. Javascript is written in assets/js/custom.js
 3. There is 1 bin2text function which is written in functions.php file
 4. Composer works, can add packages. But there was no need.
+5. For adding new route-view: create a folder or file inside routes folder. 
+The the url will be, for example: /?route=folderName/fileName.php
+6. Database sql file is in database folder
--- routes/index.php
+++ routes/index.php
@@ -9,21 +9,22 @@
     <h4>Installation</h4>
 
     <ul>
-        <li>1. Open config/database.php file and replace database credentials with yours</li>
-        <li>2. Start install.php file (Example: http://yoursite.com/install.php) or <a href="install.php">Click Here</a></li>
-        <li>3. For replacing default credentials you can open config/app.php file and edit them</li>
+        <li>1. Open <b>config/database.php</b> file and replace database credentials with yours</li>
+        <li>2. Start install.php file (Example: <b>http://yoursite.com/install.php</b>) or <a href="install.php">Click Here</a></li>
+        <li>3. For replacing default credentials you can open <b>config/app.php</b> file and edit them</li>
     </ul>
 
     <h4>What and Where</h4>
     <ul>
         <li>Routes are placed in routes folder file by file in related folder: index, task1, task2, task3</li>
-        <li>Javascript is written in assets/js/custom.js</li>
+        <li>Javascript is written in <b>assets/js/custom.js</b></li>
         <li>There are bin2text and config functions which are written in functions.php file</li>
         <li>Composer works, can add packages. But there was no need.</li>
         <li>
             For adding new route-view: create a folder or file inside routes folder.
             <br/>
-            The the url will be, for example: /route=folderName/fileName.php
+            The the url will be, for example: <b>/?route=folderName/fileName.php</b>
         </li>
+        <li>Database sql file is in <b>database</b> folder</li>
     </ul>
 </div><!-- /.container -->
